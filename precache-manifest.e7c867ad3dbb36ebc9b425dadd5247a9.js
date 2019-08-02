ea0f07053ccc3157808dbea9d7769841ded7dfe6
Nicola Zambello <nzambello@protonmail.com>
2019-08-02 13:08:15 +0200

    Updates

---
+++ precache-manifest.e7c867ad3dbb36ebc9b425dadd5247a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57a4df1de5a89af654022a96c7fdeb8",
+    "url": "/test-leaflet-geosearch/index.html"
+  },
+  {
+    "revision": "2d379717ba45759a8ce4",
+    "url": "/test-leaflet-geosearch/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a202699e16457d075168",
+    "url": "/test-leaflet-geosearch/static/js/2.a651859a.chunk.js"
+  },
+  {
+    "revision": "2d379717ba45759a8ce4",
+    "url": "/test-leaflet-geosearch/static/js/main.6eb86240.chunk.js"
+  },
+  {
+    "revision": "00409079b16dbb3f82a6",
+    "url": "/test-leaflet-geosearch/static/js/runtime~main.3b7e0ed8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-leaflet-geosearch/static/media/logo.5d5d9eef.svg"
+  }
+]);
